b5b39d92d943295f917975055d052ac9f83c5d41
nihureha <47959539+nihureha@users.noreply.github.com>
2024-09-26 21:36:31 -0700

    Added a Player that moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private PlayerInputActions playerControls;
+    private InputAction move;
+    private Vector2 moveDirection;
+    [SerializeField] private float speed = 0;
+
+    private void Awake() {
+        playerControls = new PlayerInputActions();
+    }
+    private void OnEnable() {
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = move.ReadValue<Vector2>();
+    }
+    private void FixedUpdate() {
+        GetComponent<Rigidbody2D>().velocity = moveDirection * speed;
     }
 }
